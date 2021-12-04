c1a32826ecdd2abe48dcbec3b9ec35014e3500b3
Justin D-Z <justindz@gmail.com>
2021-12-04 15:04:12 -0500

    Added level 5 weapons and armor.

--- weapons.py
+++ weapons.py
@@ -48,6 +48,30 @@ weapons = {
         'crit_damage': 0,
         'required_strength': 6
     },
+    'iron_hammer': {
+        'name': 'Iron Hammer',
+        'description': 'TODO',
+        'level': 5,
+        'weight': 5,
+        '_weapon_type': WeaponType.hammer.value,
+        'bonus_strength': 8,
+        'base_crit_chance': 0.05,
+        'damages': [[2, 4, Elements.earth.value]],
+        'crit_damage': 0,
+        'required_strength': 8
+    },
+    'iron_warhmammer': {
+        'name': 'Iron Warhammer',
+        'description': 'TODO',
+        'level': 5,
+        'weight': 10,
+        '_weapon_type': WeaponType.hammer.value,
+        'bonus_strength': 8,
+        'base_crit_chance': 0.05,
+        'damages': [[2, 6, Elements.earth.value]],
+        'crit_damage': 0,
+        'required_strength': 12
+    },
     # Sword/Int
     'rusty_longsword': {
         'name': 'Rusty Longsword',
@@ -58,7 +82,7 @@ weapons = {
         'bonus_intelligence': 2,
         'base_crit_chance': 0.05,
         'damages': [[1, 4, Elements.earth.value]],
-        'crit_damage': 0
+        'crit_damage': 0,
     },
     'rusty_greatsword': {
         'name': 'Rusty Greatsword',
@@ -66,7 +90,7 @@ weapons = {
         'level': 1,
         'weight': 8,
         '_weapon_type': WeaponType.sword.value,
-        'bonus_intelligence': 4,
+        'bonus_intelligence': 2,
         'base_crit_chance': 0.05,
         'damages': [[1, 6, Elements.earth.value]],
         'crit_damage': 0
@@ -80,7 +104,8 @@ weapons = {
         'bonus_intelligence': 4,
         'base_crit_chance': 0.05,
         'damages': [[1, 4, Elements.earth.value]],
-        'crit_damage': 0
+        'crit_damage': 0,
+        'required_intelligence': 4
     },
     'stone_greatsword': {
         'name': 'Stone Greatsword',
@@ -88,10 +113,35 @@ weapons = {
         'level': 3,
         'weight': 10,
         '_weapon_type': WeaponType.sword.value,
-        'bonus_intelligence': 2,
+        'bonus_intelligence': 4,
         'base_crit_chance': 0.05,
         'damages': [[1, 6, Elements.earth.value]],
-        'crit_damage': 0
+        'crit_damage': 0,
+        'required_intelligence': 6
+    },
+    'iron_longsword': {
+        'name': 'Iron Longsword',
+        'description': 'TODO',
+        'level': 5,
+        'weight': 5,
+        '_weapon_type': WeaponType.sword.value,
+        'bonus_intelligence': 8,
+        'base_crit_chance': 0.05,
+        'damages': [[2, 4, Elements.earth.value]],
+        'crit_damage': 0,
+        'required_intelligence': 8
+    },
+    'iron_greatsword': {
+        'name': 'Iron Greatsword',
+        'description': 'TODO',
+        'level': 5,
+        'weight': 10,
+        '_weapon_type': WeaponType.sword.value,
+        'bonus_intelligence': 8,
+        'base_crit_chance': 0.05,
+        'damages': [[2, 6, Elements.earth.value]],
+        'crit_damage': 0,
+        'required_intelligence': 12
     },
     # Dagger/Dex
     'rusty_shiv': {
@@ -125,7 +175,8 @@ weapons = {
         'bonus_dexterity': 4,
         'base_crit_chance': 0.08,
         'damages': [[1, 3, Elements.earth.value]],
-        'crit_damage': 0
+        'crit_damage': 0,
+        'required_dexterity': 4
     },
     'stone_dagger': {
         'name': 'Stone Dagger',
@@ -138,6 +189,30 @@ weapons = {
         'damages': [[1, 5, Elements.earth.value]],
         'crit_damage': 0,
         'required_dexterity': 6
+    },
+    'iron_shiv': {
+        'name': 'Iron Shiv',
+        'description': 'TODO',
+        'level': 5,
+        'weight': 3,
+        '_weapon_type': WeaponType.dagger.value,
+        'bonus_dexterity': 4,
+        'base_crit_chance': 0.08,
+        'damages': [[2, 3, Elements.earth.value]],
+        'crit_damage': 0,
+        'required_dexterity': 8
+    },
+    'iron_dagger': {
+        'name': 'Iron Dagger',
+        'description': 'TODO',
+        'level': 5,
+        'weight': 4,
+        '_weapon_type': WeaponType.dagger.value,
+        'bonus_dexterity': 4,
+        'base_crit_chance': 0.07,
+        'damages': [[2, 5, Elements.earth.value]],
+        'crit_damage': 0,
+        'required_dexterity': 12
     },
     # Staff/Will
     'rusty_scepter': {
@@ -186,6 +261,30 @@ weapons = {
         'crit_damage': 0,
         'required_willpower': 6
     },
+    'iron_scepter': {
+        'name': 'Iron Scepter',
+        'description': 'TODO',
+        'level': 5,
+        'weight': 4,
+        '_weapon_type': WeaponType.staff.value,
+        'bonus_willpower': 8,
+        'base_crit_chance': 0.05,
+        'damages': [[2, 3, Elements.earth.value]],
+        'crit_damage': 0,
+        'required_willpower': 8
+    },
+    'iron_staff': {
+        'name': 'Iron Staff',
+        'description': 'TODO',
+        'level': 5,
+        'weight': 8,
+        '_weapon_type': WeaponType.staff.value,
+        'bonus_willpower': 8,
+        'base_crit_chance': 0.05,
+        'damages': [[2, 5, Elements.earth.value]],
+        'crit_damage': 0,
+        'required_willpower': 12
+    },
     # Axe/Str+Int
     'rusty_war_axe': {
         'name': 'Rusty War Axe',
@@ -229,7 +328,7 @@ weapons = {
 
     },
     'stone_battle_axe': {
-        'name': 'STone Battle Axe',
+        'name': 'Stone Battle Axe',
         'description': 'TODO',
         'level': 3,
         'weight': 10,
@@ -241,6 +340,36 @@ weapons = {
         'crit_damage': 0,
         'required_strength': 3,
         'required_intelligence': 3,
+
+    },
+    'iron_war_axe': {
+        'name': 'Iron War Axe',
+        'description': 'TODO',
+        'level': 5,
+        'weight': 5,
+        '_weapon_type': WeaponType.axe.value,
+        'bonus_strength': 4,
+        'bonus_intelligence': 4,
+        'base_crit_chance': 0.05,
+        'damages': [[2, 4, Elements.earth.value]],
+        'crit_damage': 0,
+        'required_strength': 5,
+        'required_intelligence': 5,
+
+    },
+    'iron_battle_axe': {
+        'name': 'Iron Battle Axe',
+        'description': 'TODO',
+        'level': 5,
+        'weight': 10,
+        '_weapon_type': WeaponType.axe.value,
+        'bonus_strength': 5,
+        'bonus_intelligence': 5,
+        'base_crit_chance': 0.05,
+        'damages': [[2, 6, Elements.earth.value]],
+        'crit_damage': 0,
+        'required_strength': 6,
+        'required_intelligence': 6,
 
     },
     # Spear/Str+Dex
@@ -296,6 +425,34 @@ weapons = {
         'required_strength': 3,
         'required_dexterity': 3
     },
+    'iron_spear': {
+        'name': 'Iron Spear',
+        'description': 'TODO',
+        'level': 5,
+        'weight': 6,
+        '_weapon_type': WeaponType.spear.value,
+        'bonus_strength': 4,
+        'bonus_dexterity': 4,
+        'base_crit_chance': 0.06,
+        'damages': [[2, 4, Elements.earth.value]],
+        'crit_damage': 0,
+        'required_strength': 5,
+        'required_dexterity': 5
+    },
+    'iron_lance': {
+        'name': 'Iron Lance',
+        'description': 'TODO',
+        'level': 5,
+        'weight': 10,
+        '_weapon_type': WeaponType.spear.value,
+        'bonus_strength': 5,
+        'bonus_dexterity': 5,
+        'base_crit_chance': 0.06,
+        'damages': [[1, 6, Elements.earth.value]],
+        'crit_damage': 0,
+        'required_strength': 6,
+        'required_dexterity': 6
+    },
     # Flail/Str+Will
     'rusty_morning_star': {
         'name': 'Rusty Morning Star',
@@ -348,6 +505,34 @@ weapons = {
         'crit_damage': 0,
         'required_strength': 3,
         'required_willpower': 3
+    },
+    'iron_morning_star': {
+        'name': 'Iron Morning Star',
+        'description': 'TODO',
+        'level': 5,
+        'weight': 6,
+        '_weapon_type': WeaponType.flail.value,
+        'bonus_strength': 4,
+        'bonus_willpower': 4,
+        'base_crit_chance': 0.05,
+        'damages': [[2, 4, Elements.earth.value]],
+        'crit_damage': 0,
+        'required_strength': 5,
+        'required_willpower': 5
+    },
+    'iron_flail': {
+        'name': 'Iron Flail',
+        'description': 'TODO',
+        'level': 5,
+        'weight': 10,
+        '_weapon_type': WeaponType.flail.value,
+        'bonus_strength': 5,
+        'bonus_willpower': 5,
+        'base_crit_chance': 0.05,
+        'damages': [[2, 6, Elements.earth.value]],
+        'crit_damage': 0,
+        'required_strength': 6,
+        'required_willpower': 6
     },
     # Fist/Int+Dex
     'rusty_katar': {
@@ -402,7 +587,34 @@ weapons = {
         'crit_damage': 0,
         'required_intelligence': 3,
         'required_dexterity': 3,
-
+    },
+    'iron_katar': {
+        'name': 'Iron Katar',
+        'description': 'TODO',
+        'level': 5,
+        'weight': 4,
+        '_weapon_type': WeaponType.fist.value,
+        'bonus_intelligence': 4,
+        'bonus_dexterity': 4,
+        'base_crit_chance': 0.06,
+        'damages': [[2, 4, Elements.earth.value]],
+        'crit_damage': 0,
+        'required_intelligence': 5,
+        'required_dexterity': 5
+    },
+    'iron_claw': {
+        'name': 'Iron Claw',
+        'description': 'TODO',
+        'level': 5,
+        'weight': 5,
+        '_weapon_type': WeaponType.fist.value,
+        'bonus_intelligence': 5,
+        'bonus_dexterity': 5,
+        'base_crit_chance': 0.06,
+        'damages': [[2, 6, Elements.earth.value]],
+        'crit_damage': 0,
+        'required_intelligence': 6,
+        'required_dexterity': 6,
     },
     # Magic/Int+Will
     'rough_wand': {
@@ -462,6 +674,36 @@ weapons = {
         'required_intelligence': 3,
         'required_willpower': 3
     },
+    'carved_wand': {
+        'name': 'Carved Wand',
+        'description': 'TODO',
+        'level': 5,
+        'weight': 2,
+        '_weapon_type': WeaponType.magic.value,
+        'bonus_intelligence': 4,
+        'bonus_willpower': 4,
+        'bonus_mana': 25,
+        'base_crit_chance': 0.05,
+        'damages': [[2, 2, Elements.earth.value]],
+        'crit_damage': 7,
+        'required_intelligence': 4,
+        'required_willpower': 4
+    },
+    'glowing_orb': {
+        'name': 'Glowing Orb',
+        'description': 'TODO',
+        'level': 5,
+        'weight': 3,
+        '_weapon_type': WeaponType.magic.value,
+        'bonus_intelligence': 4,
+        'bonus_willpower': 4,
+        'bonus_mana': 50,
+        'base_crit_chance': 0.01,
+        'damages': [[2, 1, Elements.earth.value]],
+        'crit_damage': 0,
+        'required_intelligence': 4,
+        'required_willpower': 4
+    },
     # Thrown/Dex+Will
     'rusty_darts': {
         'name': 'Rusty Darts',
@@ -514,5 +756,33 @@ weapons = {
         'crit_damage': 3,
         'required_dexterity': 3,
         'required_willpower': 3,
+    },
+    'iron_darts': {
+        'name': 'Iron Darts',
+        'description': 'TODO',
+        'level': 5,
+        'weight': 3,
+        '_weapon_type': WeaponType.thrown.value,
+        'bonus_dexterity': 4,
+        'bonus_willpower': 4,
+        'base_crit_chance': 0.06,
+        'damages': [[2, 3, Elements.earth.value]],
+        'crit_damage': 5,
+        'required_dexterity': 5,
+        'required_willpower': 5
+    },
+    'iron_chakram': {
+        'name': 'Iron Chakram',
+        'description': 'TODO',
+        'level': 5,
+        'weight': 5,
+        '_weapon_type': WeaponType.thrown.value,
+        'bonus_dexterity': 5,
+        'bonus_willpower': 5,
+        'base_crit_chance': 0.06,
+        'damages': [[2, 5, Elements.earth.value]],
+        'crit_damage': 5,
+        'required_dexterity': 6,
+        'required_willpower': 6,
     },
 }
